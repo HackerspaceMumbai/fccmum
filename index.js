c0bd23616b5ee9040238990ed6a814ccc3a56efa
Augustine Correa <augcor@gmail.com>
2018-02-01 15:09:31 +0530

    1. Using cloudinary CDN for images. 2. Add service worker support 3. Adding favicon

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
 	$('.parallax').parallax();
 });
+
+navigator.serviceWorker && navigator.serviceWorker.register('sw.js').then(function(registration){
+        console.log('Excellent, registered with scope: ', registration.scope);
+});
